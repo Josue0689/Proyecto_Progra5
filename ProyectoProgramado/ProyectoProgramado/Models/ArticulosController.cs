e0efc82f60873c12d193091805c9c1d34a34d7d2
Josué Meléndez Montero <M_Mmontero@hotmail.com>
2018-12-17 14:48:29 -0600

    Estructura

---
+++ ProyectoProgramado/ProyectoProgramado/Models/ArticulosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoProgramado;
+
+namespace ProyectoProgramado.Models
+{
+    public class ArticulosController : Controller
+    {
+        private ObjetosPerdidosEntities db = new ObjetosPerdidosEntities();
+
+        // GET: Articulos
+        public ActionResult Index()
+        {
+            var articulo = db.Articulo.Include(a => a.SubCategoria1).Include(a => a.Usuario);
+            return View(articulo.ToList());
+        }
+
+        // GET: Articulos/Details/5
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulo.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // GET: Articulos/Create
+        public ActionResult Create()
+        {
+            ViewBag.SubCategoria = new SelectList(db.SubCategoria, "IdSubCategoria", "NombreSubCategoria");
+            ViewBag.IngresadoPor = new SelectList(db.Usuario, "IdUser", "Nombre");
+            return View();
+        }
+
+        // POST: Articulos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdArticulo,SubCategoria,Descripcion,FechaRecibido,Estado,FechaEntrega,NombreDueno,IngresadoPor")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articulo.Add(articulo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.SubCategoria = new SelectList(db.SubCategoria, "IdSubCategoria", "NombreSubCategoria", articulo.SubCategoria);
+            ViewBag.IngresadoPor = new SelectList(db.Usuario, "IdUser", "Nombre", articulo.IngresadoPor);
+            return View(articulo);
+        }
+
+        // GET: Articulos/Edit/5
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulo.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.SubCategoria = new SelectList(db.SubCategoria, "IdSubCategoria", "NombreSubCategoria", articulo.SubCategoria);
+            ViewBag.IngresadoPor = new SelectList(db.Usuario, "IdUser", "Nombre", articulo.IngresadoPor);
+            return View(articulo);
+        }
+
+        // POST: Articulos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdArticulo,SubCategoria,Descripcion,FechaRecibido,Estado,FechaEntrega,NombreDueno,IngresadoPor")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(articulo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.SubCategoria = new SelectList(db.SubCategoria, "IdSubCategoria", "NombreSubCategoria", articulo.SubCategoria);
+            ViewBag.IngresadoPor = new SelectList(db.Usuario, "IdUser", "Nombre", articulo.IngresadoPor);
+            return View(articulo);
+        }
+
+        // GET: Articulos/Delete/5
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulo.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articulos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            Articulo articulo = db.Articulo.Find(id);
+            db.Articulo.Remove(articulo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoProgramado/ProyectoProgramado/Models/UsuariosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoProgramado;
+
+namespace ProyectoProgramado.Models
+{
+    public class UsuariosController : Controller
+    {
+        private ObjetosPerdidosEntities db = new ObjetosPerdidosEntities();
+
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View(db.Usuario.ToList());
+        }
+
+        // GET: Usuarios/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Usuarios/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdUser,Nombre,Apellido,Email,Telefono,Usuario1,Contraseña,IdTipoUsuario")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario.IdUser = Guid.NewGuid();
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdUser,Nombre,Apellido,Email,Telefono,Usuario1,Contraseña,IdTipoUsuario")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        // GET: Usuarios/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        // POST: Usuarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            Usuario usuario = db.Usuario.Find(id);
+            db.Usuario.Remove(usuario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
