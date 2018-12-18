b39086c1a92f1aa216c5f959ab944300a18d4ff0
Josué Meléndez Montero <M_Mmontero@hotmail.com>
2018-12-18 16:26:09 -0600

    Commit

---
+++ ProyectoProgramado/ProyectoProgramadoWCF/ServiceUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ProyectoProgramadoWCF
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServiceUser" en el código y en el archivo de configuración a la vez.
+    public class ServiceUser : IServiceUser
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
